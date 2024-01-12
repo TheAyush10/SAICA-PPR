0f89f73f2c279988be8dc7ca8d614cc4adfec918
Karmavir Joshi <12084147+kvjoshi@users.noreply.github.com>
2024-01-12 16:57:28 +0530

    Initial Commit with soruce code

---
+++ source/app/src/main/java/com/powerappsportals/saicappr/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.powerappsportals.saicappr.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
